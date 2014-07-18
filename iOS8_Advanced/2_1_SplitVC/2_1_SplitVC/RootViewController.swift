c958d11957c017b505b81c815ae550e2eebd68ea
msyk <nii@msyk.net>
2014-07-18 16:10:36 +0900

    Add Split View Sample

---
+++ iOS8_Advanced/2_1_SplitVC/2_1_SplitVC/RootViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RootViewController.swift
+//  2_1_SplitVC
+//
+//  Created by 新居雅行 on 2014/07/18.
+//  Copyright (c) 2014年 msyk.net. All rights reserved.
+//
+
+import Cocoa
+
+class RootViewController: UIViewController {
+
+}
