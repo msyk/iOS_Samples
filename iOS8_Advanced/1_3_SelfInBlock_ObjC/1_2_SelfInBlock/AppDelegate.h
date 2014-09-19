a208c9648bd782571d5942760af4c04ab7f77d73
msyk <nii@msyk.net>
2014-09-19 13:10:06 +0900

    Missing project

---
+++ iOS8_Advanced/1_3_SelfInBlock_ObjC/1_2_SelfInBlock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  1_2_SelfInBlock
+//
+//  Created by 新居雅行 on 2014/07/11.
+//  Copyright (c) 2014年 msyk.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS8_Advanced/1_3_SelfInBlock_ObjC/1_2_SelfInBlock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  1_2_SelfInBlock
+//
+//  Created by 新居雅行 on 2014/07/11.
+//  Copyright (c) 2014年 msyk.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
