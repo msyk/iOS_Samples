1839e3b1a694a1c5ff1a9dba45b360657176f050
msyk <nii@msyk.net>
2015-01-28 21:13:46 +0900

    Initial Commit

--- iOS8_Adv_Training/Table4_Swift/Table4_Swift/MasterViewController.swift
+++ iOS8_Adv_Training/Table4_Swift/Table4_Swift/MasterViewController.swift
@@ -22,29 +22,30 @@ class MasterViewController: UITableViewController {
         }
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    self.navigationItem.leftBarButtonItem = self.editButtonItem()
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
-        if let split = self.splitViewController {
-            let controllers = split.viewControllers
-            self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
-        }
+    let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+    self.navigationItem.rightBarButtonItem = addButton
+    if let split = self.splitViewController {
+        let controllers = split.viewControllers
+        self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
     }
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    func insertNewObject(sender: AnyObject) {
-        objects.insertObject(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-    }
+func insertNewObject(sender: AnyObject) {
+    objects.insertObject(NSDate(), atIndex: 0)
+    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+    self.tableView.insertRowsAtIndexPaths(
+        [indexPath], withRowAnimation: .Automatic)
+}
 
     // MARK: - Segues
 
@@ -78,20 +79,33 @@ class MasterViewController: UITableViewController {
         return cell
     }
 
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+override func tableView(tableView: UITableView,
+    canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
-    }
+}
 
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+override func tableView(tableView: UITableView,
+    commitEditingStyle editingStyle: UITableViewCellEditingStyle,
+    forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             objects.removeObjectAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
-    }
+}
+override func tableView(tableView: UITableView,
+    moveRowAtIndexPath sourceIndexPath: NSIndexPath,
+    toIndexPath destinationIndexPath: NSIndexPath) {
+        println("fromIndex=\(sourceIndexPath),",
+            " toIndex=\(destinationIndexPath)")
+}
 
+override func tableView(tableView: UITableView,
+    canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+}
 
 }
 
