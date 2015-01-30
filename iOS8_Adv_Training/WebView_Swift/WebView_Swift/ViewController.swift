aee801167dc2523195e03f9fe1c61fefb305bdc3
msyk <nii@msyk.net>
2015-01-30 16:26:42 +0900

    Initial Commit

--- iOS8_Adv_Training/WebView_Swift/WebView_Swift/ViewController.swift
+++ iOS8_Adv_Training/WebView_Swift/WebView_Swift/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIWebViewDelegate {
+    
+    @IBOutlet private weak var webView: UIWebView?
+    
+    @IBAction private func tapButton1(sender: UIButton) {
+        let js = "document.getElementById('obj').style.backgroundColor='orange';"
+        webView?.stringByEvaluatingJavaScriptFromString(js)
+    }
+    
+    @IBAction private func tapButton2(sender: UIButton) {
+        let js = "document.getElementById('image').src;"
+        let returnString = webView?
+            .stringByEvaluatingJavaScriptFromString(js)
+        println("returnString = \(returnString)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let baseURL = NSBundle.mainBundle().resourceURL;
+        let htmlString = "<html>"
+            + "<body style='margin:0;padding:0;background-color:red'>"
+            + "<div id='obj'>OBJ-DIV</div>"
+            + "<div id='link'><a href='http://mac.com/'>Site</a></div>"
+            + "<img id='image' src='photo1.jpg'>"
+            + "<div id='tap'><a href='taplink://?id=100&p=200'>Tap Here</a></div>"
+            + "</body></html>"
+        
+        webView?.loadHTMLString(htmlString, baseURL: baseURL)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        println(__FUNCTION__)
+    }
+    
+    func webView(webView: UIWebView,
+        shouldStartLoadWithRequest request: NSURLRequest,
+        navigationType: UIWebViewNavigationType) -> Bool {
+            println(__FUNCTION__)
+            println("request = \(request)")
+            println("navigationTyle = \(navigationType)")
+            return true
+    }
+    
 }
 
