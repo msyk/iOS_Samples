97930f33272f4ab122a2171472f375e3ed972073
Masayuki Nii <msyk@msyk.net>
2015-05-24 01:30:25 +0900

    iOS Book for swift 1.2 until Chapter 4

---
+++ iOS8_Advanced/6_2_PDFViewer_Swift/PDFViewer_Swift/PDFView.swift
@@ -0,0 +1,21 @@
+//
+//  PDFView.swift
+//  PDFViewer_Swift
+//
+//  Created by Masayuki Nii on 2015/05/24.
+//  Copyright (c) 2015年 msyk.net. All rights reserved.
+//
+
+import UIKit
+
+class PDFView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
